32bc87c466a707accf7493e404bd3f7abef54e9b
Meraki <130898314+sd28-k@users.noreply.github.com>
2024-09-25 13:10:17 +0530

    Update facemask.py

--- facemask.py
+++ facemask.py
@@ -68,7 +68,7 @@ model.save('mymodel.h5',model_saved)
 
 mymodel=load_model('mymodel.h5')
 #test_image=image.load_img('C:/Users/Karan/Desktop/ML Datasets/Face Mask Detection/Dataset/test/without_mask/30.jpg',target_size=(150,150,3))
-test_image=image.load_img(r'C:/Users/karan/Desktop/FaceMaskDetector/test/with_mask/1-with-mask.jpg',
+test_image=image.load_img(r'test/with_mask/1-with-mask.jpg',
                           target_size=(150,150,3))
 test_image
 test_image=image.img_to_array(test_image)
